09c8289e70329045edd22a66022f1fc65bacfea6
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2022-11-19 17:25:40 -0300

    Add files via upload
    Adicionando modificações na classe Main.

---
+++ Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+            Curso curso1 = new Curso();
+            curso1.setTitulo("curso java");
+            curso1.setDescricao("descrição curso java");
+            curso1.setCargaHoraria(8);
+    
+            Curso curso2 = new Curso();
+            curso2.setTitulo("curso javascript");
+            curso2.setDescricao("descrição curso js");
+            curso2.setCargaHoraria(4);
+    
+
+
+            Mentoria mentoria = new Mentoria();
+            mentoria.setTitulo("mentoria de java");
+            mentoria.setDescricao("descrição mentoria java");
+            mentoria.setData(LocalDate.now());
+            //System.out.println(curso1);
+            //System.out.println(curso2);
+            //System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devLuiz = new Dev();
+        devLuiz.setNome("Luiz");
+        devLuiz.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Luiz:" + devLuiz.getConteudosInscritos());
+        devLuiz.progredir();
+        devLuiz.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Camila:" + devLuiz.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila:" + devLuiz.getConteudosConcluidos());
+        System.out.println("XP:" + devLuiz.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
+    }
+
+}
+            
+
+    
